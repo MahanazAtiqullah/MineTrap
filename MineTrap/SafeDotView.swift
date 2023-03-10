59c0118b8341be34a7be6879cd7cde00b36cd532
Mahanaz Atiqullah <mhatiqullah@gmail.com>
2023-03-10 13:41:36 -0600

    dump

--- MineTrap/SafeDotView.swift
+++ MineTrap/SafeDotView.swift
@@ -13,11 +13,19 @@ class SafeDotView: UIView {
         super.init(frame: frame)
         self.backgroundColor = .systemPink
         self.layer.cornerRadius = 45
-        addGestureRecognizer(UITapGestureRecognizer())
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
+
+extension SafeDotView {
+    
+    @objc func handleTap() {
+        self.removeFromSuperview()
+    }
+}
+
